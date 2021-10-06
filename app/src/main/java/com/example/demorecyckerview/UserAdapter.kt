07f9c7da74bfcf1089be2d60a8e5eb4664215196
martamagui <marta.molina@live.u-tad.com>
2021-10-06 13:14:36 +0200

    trasteando

---
+++ app/src/main/java/com/example/demorecyckerview/UserAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.demorecyckerview
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+
+class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    }
+}
