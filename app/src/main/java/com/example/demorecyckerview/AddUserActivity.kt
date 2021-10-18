4f2adb276d5047b03bcb5e7a2a81c7e46b1e4b8b
martamagui <marta.molina@live.u-tad.com>
2021-10-18 13:54:06 +0200

    Antes de añadir imgs,  nuevos botones

--- app/src/main/java/com/example/demorecyckerview/AddUserActivity.kt
+++ app/src/main/java/com/example/demorecyckerview/AddUserActivity.kt
@@ -4,36 +4,62 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import java.util.*
 
 class AddUserActivity : AppCompatActivity() {
+    private lateinit var tilFirstName: TextInputEditText
+    private lateinit var tilLastName: TextInputEditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_user)
 
-        val app = (application as App)
-        val users = app.users
+        tilFirstName = findViewById(R.id.tied_first_name)
+        tilLastName = findViewById(R.id.tied_last_name)
+        val btnAdd: Button = findViewById(R.id.btn_add_user)
+        val btnAddExit : Button = findViewById(R.id.btn_add_and_exit)
+        val btnExit: Button = findViewById(R.id.btn_exit)
 
 
-        val tilFirstName: TextInputEditText = findViewById(R.id.tied_first_name)
-        val tilLastName: TextInputEditText = findViewById(R.id.tied_last_name)
-        val btnAdd: Button =  findViewById(R.id.btn_add_user)
-
-
-
-        btnAdd.setOnClickListener{
-            val firstName: String = tilFirstName.text.toString()
-            val lastName: String = tilLastName.text.toString()
-            users.add(User(UUID.randomUUID().toString(),firstName,lastName,""))
-
-            //cambio de pantalla
-            val nextScreenIntent: Intent = Intent(this, MainActivity::class.java)
-            startActivity(nextScreenIntent)
-
+        btnAdd.setOnClickListener {
+            if (addUser()) {
+                tilFirstName.text=null
+                tilLastName.text=null
+                //Obligar a que el cursor vuelva al campo superior
+                tilFirstName.requestFocus()
+            }
+        }
+        btnAddExit.setOnClickListener{
+            if (addUser()) {
+                finish()
+            }
+        }
+        btnExit.setOnClickListener{
+            finish()
         }
 
+    }
+    private fun addUser():Boolean{
+        val firstName: String = tilFirstName.text.toString()
+        val lastName: String = tilLastName.text.toString()
+        if (firstName.length > 3 && lastName.length > 3) {
+            (application as App).users.add(
+                User(
+                    UUID.randomUUID().toString(),
+                    firstName,
+                    lastName,
+                    ""
+                )
+            )
+            return true
+        } else {
+            Toast.makeText(this, "Datos requeridos", Toast.LENGTH_SHORT).show()
+            return false
+        }
     }
 }
 
--- app/src/main/java/com/example/demorecyckerview/MainActivity.kt
+++ app/src/main/java/com/example/demorecyckerview/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Adapter
 import android.widget.Button
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,6 +12,8 @@ import androidx.recyclerview.widget.RecyclerView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         val app = (application as App)
         val users = app.users
 
-        val adapter =
+        adapter =
             UserAdapter(users) { user -> Log.d("MainActivivty", "Userclicked on: $user") }
         rvUser.adapter = adapter
         //val layoutManager = LinearLayoutManager(this)
@@ -59,5 +62,10 @@ class MainActivity : AppCompatActivity() {
                 "Image $index"
             )
         }.toMutableList()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.notifyDataSetChanged()
     }
 }
